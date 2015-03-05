200c5e8cbf7f3ac48bd3427d1a2a523cda321d40
cronjaeger <kitkat2.0@gmail.com>
2015-03-05 22:31:59 +0000

    Implemented and did initial tests on euler_maruyama_multilevel.

--- mlmc-pkg/R/euler_maruyama.R
+++ mlmc-pkg/R/euler_maruyama.R
@@ -23,9 +23,94 @@ euler_maruyama <- function(t_max,h,drift,dispersion,X0){
   return(list(t=steps,X=X))
 }
 
-euler_maruyama_multilevel <- function(L,M,h0,Tmax,NL = 10^4,a,b,S0)
-  #implement stuff
-  return(P_fine -P_coarse)
+#' An auxiliary function for computing P_L - P_(L-1)
+#'
+#' @description simulates N_L independent paths of the SDE
+#'
+#' dS_t = a(t,S_t) dt + b(t,S_t) dW_t
+#'
+#' on two differnt grids -- one grid being finer than the
+#' other by a factor of M. Each path on the coarse grid is generated
+#' by summing noise from the finer grid.
+#' The estimates of payoffFunction(S_Tmax) on the two different grids
+#' is then compared and returned for each pair of paths
+#'
+#' @param L The level of the finest grid to be considered.
+#' Should be a positive integer
+#' @param M The factor of refinement between two levels.
+#' Should be a positive integer
+#' @param h0 The step size used at level 0. At level L, the step size
+#' is given by h0*M^-L
+#' @param Tmax the time-horizon of simulations. The underlying random
+#' variable is simulated on the time-interval [0,Tmax]
+#' @param N_L the number of paths to be sampled.
+#' @param a the coefficient of drift in the diffusion
+#' @param b the coefficient of dispersion in the diffusion
+#' @param S0 the initial condition of the diffusion
+#' @param payoffFunction the payoffFunction to be used
+#'
+#' @return A vector of length \code{N_L}. Each entry containing 1
+#' estimate of the difference in discretisation-error between two
+#' consecutive grids.
+euler_maruyama_multilevel <- function(
+    L,
+    M,
+    h0,
+    Tmax,
+    N_L = 10^4,
+    a,
+    b,
+    S0,
+    payoffFunction = function(S) S)
+  {
+
+  #infer implicitly specified parameters
+  h_fine <- h0 * M^(-L)
+  h_coarse <- h0 * M^(-L + 1)
+  n_steps_fine <- Tmax/h_fine
+  n_steps_coarse <- round(n_steps_fine/M)
+
+  #precompute increments
+  increments <- rnorm(N_L * n_steps_fine , sd = sqrt(h_fine))
+  dim(increments) <- c(N_L,M,n_steps_coarse)
+
+  #initialize result-vectors
+  S_coarse <- rep(0.0,N_L)
+  S_fine <- rep(0.0,N_L)
+
+  #define reduction-steps
+  reduction_step_coarse <- function(increments){
+    #axis 2 of increments indexes 1:n_steps_coarse
+    #axis 1 of increments indexes 1:M
+    dW <- apply(increments,MARGIN = 2,FUN = sum)
+    t <- 0
+    S <- S0
+    for(i in 1:n_steps_coarse){
+      t <- i*h_coarse
+      S <- S + a(t,S)*h_coarse + b(t,S)*dW[i]
+    }
+    return(S)
+  }
+  reduction_step_fine <- function(increments){
+    dim(increments) <- n_steps_fine
+    t <- 0
+    S <- S0
+    for(i in 1:n_steps_fine){
+      t <- i*h_fine
+      S <- S + a(t,S)*h_fine + b(t,S)*increments[i]
+    }
+    return(S)
+  }
+
+  #apply reduction-step to "N_L"-axis.
+  S_coarse <- apply(X = increments, MARGIN = 1, FUN = reduction_step_coarse)
+  S_fine <- apply(X = increments, MARGIN = 1, FUN = reduction_step_fine)
+
+  P_fine <- payoffFunction(S_fine)
+  P_coarse <- payoffFunction(S_coarse)
+
+  return(P_fine - P_coarse)
+}
   #function(t_max,h_fine,h_coarse,drift,dispersion,X0){
 #   #infer implicit parameters
 #   M <- round(h_fine/h_coarse)
@@ -41,7 +126,7 @@ euler_maruyama_multilevel <- function(L,M,h0,Tmax,NL = 10^4,a,b,S0)
 #   X_coarse <- c(X0,rep(NA,N_steps))
 #
 #   #TODO: finiish this
-}
+#}
 
 #' A script for testing the EM-mathod
 #' @description Simulates a geometric brownian motion, and plots the results.
@@ -66,7 +151,7 @@ testEM <- function(N_paths = 20,t_max = 1, h = 0.01){
   #output is stored as a list of lists
   tests <- list()
   maxVal <- 1 #minimum of plotting
-  minVal <- 0.9 #makimum for plotting
+  minVal <- 0.9 #maximum for plotting
   for(i in 1:N_paths){
     result <- euler_maruyama(t_max = t_max,h=h,drift = mu,dispersion = sigma,X0 = X0)
     tests <- c(tests,list(result))
