3f74525ec4f45333b02163c8f4cb8d3ca8c46d81
cronjaeger <kitkat2.0@gmail.com>
2015-03-08 16:35:55 +0000

    added "examples.R" -- contains constructors for the geometric brownian motion as well as a range of payoff functions.

--- mlmc-pkg/R/examples.R
+++ mlmc-pkg/R/examples.R
@@ -1,1 +1,34 @@
 ## Contains a range of different SDE-models and payoff-functions
+
+SDE_geomBM <- function(r = 0.05,s = 0.2,X0=1.0) list(r=r,s=s,X0=X0,mu = function(t,x) r*x,sigma = function(t,x) s*x)
+
+f_europeanOption <- function(r = 0.05, X0 = 1.0,tMax){
+  function(X_T) return(exp(-r*tMax)*max(0,X_T - X0))o
+}
+
+f_asianOption <- function(r = 0.05 , X0 = 1.0){
+  function(timesteps,X){
+    N <- length(X)
+    tMax <- timesteps[N]
+    Xbar <- 0.5 * sum((X[-1] + X[-N]) * (timesteps[-1] - timesteps[-N])))
+    return(exp(-r*tMax)*max(0,X-X0))
+  }
+}
+
+f_loopbackOption <- function(r = 0.05, s = 0.2, X0 = 1.0){
+  function(timestep, X){
+    N <- length(X)
+    tMax <- timesteps[N]
+    minX <- min(X)
+    h_l <- tMax/(N-1)
+    betaStar <- 0.5826
+    minX <- minX * (1 - betaStar*s*sqrt(h_l))
+    return(exp(-r*tMax) * (X[N] - minX))
+  }
+}
+
+f_digitalOption <- function(r = 0.05 , X0 = 1.0,tMax = 1.0){
+  function(X_T){
+    exp(-r*tMax)*as.numeric(XT > X0)
+  }
+}
